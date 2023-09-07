4510d22efd6ea9a76c4cc528619f710f9d1bf45b
Sadnan Islam <sadnan15916q@gmail.com>
2023-09-07 20:38:22 +0600

    day 1 finish

---
+++ day_1.py
@@ -0,0 +1,6 @@
+print('Welcome to the Band Generator.')
+
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's the name of you pet?\n")
+
+print(f'Your band name could be {city} {pet}')
