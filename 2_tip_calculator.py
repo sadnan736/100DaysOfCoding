71fb8551d5b118bffe44d0b834075ee5be7dbc98
Sadnan Islam <sadnan15916q@gmail.com>
2023-09-09 01:46:21 +0600

    day 2- finish

---
+++ 2_tip_calculator.py
@@ -0,0 +1,10 @@
+print("Welcome to the Tip Calcutator!")
+bill = float(input("What was the total bill? ৳"))
+persons = int(input("How many people will split the bill? "))
+tip = float((input("How much tip would you like to give? Standard is 15% ")))
+
+
+pperson_pay = (bill+ (bill*tip/100))/persons
+pperson_pay = round(pperson_pay, 2)
+
+print(f"Each person should pay: ৳{pperson_pay}")
